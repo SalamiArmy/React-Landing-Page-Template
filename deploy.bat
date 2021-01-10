4a85707af705a0d9c9f224f0743b6e1837590be4
Ashley Lewis <ultrakiff@gmail.com>
2021-01-10 20:09:40 +0200

    wrong ports

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,1 @@
-docker run -d --rm --name my-running-landing-page --platform=linux -p 5000:80 docker.pkg.github.com/salamiarmy/react-landing-page-template/image
+docker run -d --rm --name my-running-landing-page --platform=linux -p 80:5000 docker.pkg.github.com/salamiarmy/react-landing-page-template/image

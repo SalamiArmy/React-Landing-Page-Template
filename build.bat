62ace2c9830ca1529098644706fbed04ad2078f2
Ashley Lewis <ultrakiff@gmail.com>
2021-01-10 18:52:56 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 npm install "%~dp0package.json"
-npm run build
+npm run --prefix "%~dp0" build
 docker build -t docker.pkg.github.com/salamiarmy/react-landing-page-template/image --platform=linux "%~dp0."

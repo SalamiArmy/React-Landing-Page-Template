9b533a658b29c8a241231e716a9b132dd23e6027
Ashley Lewis <ultrakiff@gmail.com>
2021-01-10 23:00:07 +0200

    change port to 8000

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,1 @@
-docker run -d --restart unless-stopped --name my-running-landing-page --platform=linux -p 8080:5000 docker.pkg.github.com/salamiarmy/react-landing-page-template/image
+docker run -d --restart unless-stopped --name my-running-landing-page --platform=linux -p 8000:5000 docker.pkg.github.com/salamiarmy/react-landing-page-template/image

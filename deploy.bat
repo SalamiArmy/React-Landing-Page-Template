582652a3b8ec665a0b88f24a7d09691ff6c540de
Ashley Lewis <ultrakiff@gmail.com>
2021-01-10 15:57:20 +0200

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,1 @@
-docker run -d --rm --name my-running-landing-page --platform=linux -p 80:80 landing-page
+docker run -d --rm --name my-running-landing-page --platform=linux -p 80:80 docker.pkg.github.com/salamiarmy/react-landing-page-template/image
